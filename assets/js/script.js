2a5c4d41b205352ea5bd7d0fad4110bc504c1ce1
RH-devs <rasheedatajiborisha@gmail.com>
2021-08-20 22:30:54 +0000

    Add code to division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,11 +37,11 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
-    } else {
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
+     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
-
-
     }
 }
 /**
@@ -83,6 +83,8 @@ function calculateCorrectAnswer() {
 
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
     } else {
 
         alert(`Unimplemented operator ${operator}`);
@@ -126,10 +128,14 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 }
 
-function displayMultiplyQuestion(operand1, operand2) {
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
+}
 
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "x";
-
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "/";
 }
